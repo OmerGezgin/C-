13af1fb97266e1e5881daffb34ae575af03c291b
Ömer Gezgin <123639939+OmerGezgin@users.noreply.github.com>
2023-03-03 22:51:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vize,final; // Vize ve finali değişken olarak tanımladım
+            double ortalama; // Ortalamayı değişken olarak tanımladım.
+
+            Console.WriteLine("Vize notunu giriniz :");
+
+            vize=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Final notunu giriniz :");
+
+            final=Convert.ToInt32(Console.ReadLine());
+
+            ortalama = (vize * 0.4) + (final * 0.6); // Vize ve finalin hesaplamasını yaptırdım.
+
+            Console.WriteLine("Ortalama :"+ ortalama); // Ortalamayı çıktı olarak verdim.
+
+            Console.ReadLine(); // BİTİR.
+
+
+        }
+    }
+}
