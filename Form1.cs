aac1a612571856872b8b685426c12241d17bd89c
Ömer Gezgin <123639939+OmerGezgin@users.noreply.github.com>
2023-05-14 21:09:00 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,22 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Rastgele bir 5 haneli kod oluþturmak için randomu kullandým.
+            Random random = new Random();
+            string kod = random.Next(10000, 99999).ToString();
+
+            // Kodu MessageBox içinde gösterdim.
+            MessageBox.Show("Yeni Kodunuz: " + kod);
+        }
+    }
+}
+
+//C# FORM: Bir BUTON olacak ve buttonun üzerinde "KOD ÜRET" yazacak. Butona her basýldýðý zaman MESAJBOX içerisinde rastgele üretilmiþ 5 haneli olan kod yazacak.
